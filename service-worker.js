8efc6963b54411eb541a95ce6eb165ffbc5cc021
jan-martinek <jan-martinek@users.noreply.github.com>
2022-06-13 22:01:59 +0000

    Deploying to docs from @ books-are-next/cibula-janosik@730ceee62971d07adfdf76192ccb24fd41ac9de4 🚀

--- service-worker.js
+++ service-worker.js
@@ -764,7 +764,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 /// <reference lib="WebWorker" />
 require("regenerator-runtime");
 const sw = self;
-const CACHE = 'cache-57aa4ac';
+const CACHE = 'cache-730ceee';
 /**
  * IndexedDB singleton wrapper used to store persistent information with an predefined {@link Schema}
  */
@@ -1019,7 +1019,7 @@ async function cache(context) {
         .open(CACHE)
         .then(cache => {
         console.log('[' + context + '] Opened cache');
-        return cache.addAll(["./","./colophon.html","./favicon.png","./janosik_005.html","./janosik_006.html","./janosik_007.html","./janosik_008.html","./janosik_009.html","./janosik_010.html","./janosik_011.html","./janosik_012.html","./index.html","./janosik_013.html","./janosik_014.html","./janosik_015.html","./janosik_016.html","./janosik_017.html","./janosik_018.html","./janosik_019.html","./janosik_020.html","./janosik_021.html","./janosik_022.html","./janosik_023.html","./janosik_024.html","./janosik_025.html","./janosik_026.html","./janosik_027.html","./janosik_028.html","./janosik_029.html","./janosik_030.html","./janosik_031.html","./janosik_032.html","./manifest.json","./promo.html","./fonts/Literata-Italic-var.woff2","./fonts/Literata-var.woff2","./fonts/LiterataTT-TextItalic.woff2","./fonts/LiterataTT-TextRegular.woff2","./fonts/LiterataTT-TextSemibold.woff2","./fonts/LiterataTT_LICENSE.txt","./fonts/SpaceGroteskVF.woff2","./fonts/SpaceGroteskVF_LICENSE.txt","./resources/image001.jpg","./resources/image002.jpg","./resources/mzk_logo_tyrkys_transparent.jpg","./resources/obalka_janosik_cibula.jpg","./resources/upoutavka_eknihy.jpg","./scripts/bundle.js","./style/style.min.css","./template-images/circles.png"]);
+        return cache.addAll(["./","./colophon.html","./favicon.png","./index.html","./janosik_005.html","./janosik_006.html","./janosik_007.html","./janosik_008.html","./janosik_009.html","./janosik_010.html","./janosik_011.html","./janosik_012.html","./janosik_013.html","./janosik_014.html","./janosik_015.html","./janosik_016.html","./janosik_017.html","./janosik_018.html","./janosik_019.html","./janosik_020.html","./janosik_021.html","./janosik_022.html","./janosik_023.html","./janosik_024.html","./janosik_025.html","./janosik_026.html","./janosik_027.html","./janosik_028.html","./janosik_029.html","./janosik_030.html","./janosik_031.html","./janosik_032.html","./manifest.json","./promo.html","./fonts/Literata-Italic-var.woff2","./fonts/Literata-var.woff2","./fonts/LiterataTT-TextItalic.woff2","./fonts/LiterataTT-TextRegular.woff2","./fonts/LiterataTT-TextSemibold.woff2","./fonts/LiterataTT_LICENSE.txt","./fonts/SpaceGroteskVF.woff2","./fonts/SpaceGroteskVF_LICENSE.txt","./resources/image001.jpg","./resources/image002.jpg","./resources/mzk_logo_tyrkys_transparent.jpg","./resources/obalka_janosik_cibula.jpg","./resources/upoutavka_eknihy.jpg","./scripts/bundle.js","./style/style.min.css","./template-images/circles.png"]);
     })
         .then(() => idb.set({ key: 'cacheUpdatedAt', value: new Date().toISOString() }))
         .then(() => {
